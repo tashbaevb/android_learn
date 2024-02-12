84e28253d14ee5a0473f67e724b2a37a001157d9
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-02-12 18:17:44 +0600

    event handlers for AC and number buttons

--- app/src/main/java/com/example/first_android/CalculatorActivity.java
+++ app/src/main/java/com/example/first_android/CalculatorActivity.java
@@ -1,6 +1,8 @@
 package com.example.first_android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,38 @@ public class CalculatorActivity extends AppCompatActivity {
 
     private TextView resultTextView;
 
+    private StringBuilder input = new StringBuilder();
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.calculator);
+
+        resultTextView = findViewById(R.id.resultTextView);
+    }
+
+    public void onNumberClick(View view) {
+        Button button = (Button) view;
+        input.append(button.getText().toString());
+        updateResult();
+    }
+
+    public void onOperatorClick(View view) {
+        Button button = (Button) view;
+        input.append(button.getText().toString());
+        updateResult();
+    }
+
+    public void updateResult() {
+        resultTextView.setText(input.toString());
+    }
+
+    public void onClearClick(View view) {
+        input.setLength(0);
+        updateResult();
+    }
+
+    public void onEqualsClick(View view) {
+
     }
 }
